30abf4df8dc09b45e51e7f4a43c09df395012a0c
nommy <nomuramasam@gmail.com>
2019-07-21 16:34:29 +0900

    ユーザー認証

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/message/index', 'MessageController@index');
 Route::get('/', function () {
     return redirect('/message/index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
